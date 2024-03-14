b63d34ea111ef4e6028cae01ab08e292ee41bb10
Niveau Samuel <o22204836@info03-21.iut45.univ-orleans.fr>
2024-03-14 11:54:07 +0100

    script sql

--- SQL/creationSQL.sql
+++ SQL/creationSQL.sql
@@ -1,38 +1,37 @@
-CREATE TABLE ETAPE (
-  code_Etape int PRIMARY KEY NOT NULL AUTO_INCREMENT,
-  num_Etape  VARCHAR(42),
-  latitude   VARCHAR(42),
-  longitude  VARCHAR(42),
-  id_Tr_1    int,
-  id_Tr_2    int,
-  FOREIGN KEY (id_Tr_1) REFERENCES TRONCONS (id_Tr),
-  FOREIGN KEY (id_Tr_2) REFERENCES TRONCONS (id_Tr)
-);
+
 
 CREATE TABLE PARCOURS (
-  id_Parcours  int PRIMARY KEY NOT NULL AUTO_INCREMENT,
+  id_Parcours  NUMBER PRIMARY KEY,
   nom_Parcours VARCHAR(42)
 );
 
 CREATE TABLE TRONCONS (
-  id_Tr    int PRIMARY KEY NOT NULL AUTO_INCREMENT,
+  id_Tr    NUMBER NOT NULL PRIMARY KEY,
   nom_Tr   VARCHAR(42),
   distance VARCHAR(42),
   Type_Sol VARCHAR(42)
 );
-
+CREATE TABLE ETAPE (
+  code_Etape NUMBER NOT NULL PRIMARY KEY,
+  num_Etape  VARCHAR(42),
+  latitude   VARCHAR(42),
+  longitude  VARCHAR(42),
+  id_Tr_1    NUMBER,
+  id_Tr_2    NUMBER,
+  FOREIGN KEY (id_Tr_1) REFERENCES TRONCONS (id_Tr),
+  FOREIGN KEY (id_Tr_2) REFERENCES TRONCONS (id_Tr)
+);
 CREATE TABLE CATEGORIE (
-  id_Categorie    int PRIMARY KEY NOT NULL AUTO_INCREMENT,
+  id_Categorie    NUMBER NOT NULL PRIMARY KEY,
   nom_Cat         VARCHAR(42),
   description_Cat VARCHAR(42),
-  code_Etape      int,
+  code_Etape      NUMBER,
   FOREIGN KEY (code_Etape) REFERENCES ETAPE (code_Etape)
 );
 
 CREATE TABLE CONTENIR (
-  id_Parcours int,
-  id_Tr       int,
-  PRIMARY KEY (id_Parcours, id_Tr),
+  id_Parcours NUMBER,
+  id_Tr       NUMBER,
   FOREIGN KEY (id_Parcours) REFERENCES PARCOURS (id_Parcours),
   FOREIGN KEY (id_Tr) REFERENCES TRONCONS (id_Tr)
 );
