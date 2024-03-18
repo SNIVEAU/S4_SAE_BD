fb6cc6fdfda4747c95fb70f919fce3deccf822fa
Niveau Samuel <o22204836@info27-09.iut45.univ-orleans.fr>
2024-03-18 17:09:44 +0100

    modification bd et insert fonctionnelle

--- SQL/creationSQL.sql
+++ SQL/creationSQL.sql
@@ -1,32 +1,33 @@
-
-
 CREATE TABLE PARCOURS (
   id_Parcours  NUMBER PRIMARY KEY,
   nom_Parcours VARCHAR(42)
 );
+CREATE TABLE CATEGORIE (
+  id_Categorie    NUMBER NOT NULL PRIMARY KEY,
+  nom_Cat         VARCHAR(42),
+  description_Cat VARCHAR(42)
+);
+
+CREATE TABLE ETAPE (
+  code_Etape NUMBER NOT NULL PRIMARY KEY,
+  nom_etape  VARCHAR(42),
+  latitude   VARCHAR(42),
+  longitude  VARCHAR(42),
+  niveau_confort NUMBER,
+  FOREIGN KEY (niveau_confort) REFERENCES CATEGORIE (id_Categorie)
+);
+
 
 CREATE TABLE TRONCONS (
   id_Tr    NUMBER NOT NULL PRIMARY KEY,
   nom_Tr   VARCHAR(42),
-  distance VARCHAR(42),
-  Type_Sol VARCHAR(42)
-);
-CREATE TABLE ETAPE (
-  code_Etape NUMBER NOT NULL PRIMARY KEY,
-  num_Etape  VARCHAR(42),
-  latitude   VARCHAR(42),
-  longitude  VARCHAR(42),
-  id_Tr_1    NUMBER,
-  id_Tr_2    NUMBER,
-  FOREIGN KEY (id_Tr_1) REFERENCES TRONCONS (id_Tr),
-  FOREIGN KEY (id_Tr_2) REFERENCES TRONCONS (id_Tr)
-);
-CREATE TABLE CATEGORIE (
-  id_Categorie    NUMBER NOT NULL PRIMARY KEY,
-  nom_Cat         VARCHAR(42),
-  description_Cat VARCHAR(42),
-  code_Etape      NUMBER,
-  FOREIGN KEY (code_Etape) REFERENCES ETAPE (code_Etape)
+  distance NUMBER,
+  Type_Sol VARCHAR(42),
+  ville_tr VARCHAR(42),
+  etape_arrivee NUMBER,
+  etape_depart NUMBER,
+  FOREIGN KEY (etape_arrivee) REFERENCES ETAPE (code_Etape),
+  FOREIGN KEY (etape_depart) REFERENCES ETAPE (code_Etape)
 );
 
 CREATE TABLE CONTENIR (
--- SQL/insertSQL.sql
+++ SQL/insertSQL.sql
@@ -1,37 +1,47 @@
--- Insertion dans la table CATEGORIE
-INSERT INTO CATEGORIE (id_Categorie, nom_Cat, description_Cat, code_Etape) VALUES
-(1, 'Catégorie 1', 'Description de la catégorie 1', 'ETAPE001');
-(2, 'Catégorie 2', 'Description de la catégorie 2', 'ETAPE002');
-(3, 'Catégorie 3', 'Description de la catégorie 3', NULL);
-
--- Insertion dans la table CONTENIR
-INSERT INTO CONTENIR (id_Parcours, id_Tr) VALUES
-(1, 1),
-(1, 2),
-(2, 3);
-
--- Insertion dans la table ETAPE
-INSERT INTO ETAPE (code_Etape, num_Etape, latitude, longitude, id_Tr_1, id_Tr_2) VALUES
-('ETAPE001', 'Étape 1', 'latitude1', 'longitude1', 1, 2);
-INSERT INTO ETAPE (code_Etape, num_Etape, latitude, longitude, id_Tr_1, id_Tr_2) VALUES
-
-('ETAPE002', 'Étape 2', 'latitude2', 'longitude2', 2, 3);
-INSERT INTO ETAPE (code_Etape, num_Etape, latitude, longitude, id_Tr_1, id_Tr_2) VALUES
-('ETAPE003', 'Étape 3', 'latitude3', 'longitude3', 3, 1);
-
 -- Insertion dans la table PARCOURS
 INSERT INTO PARCOURS (id_Parcours, nom_Parcours) VALUES
 (1, 'Parcours 1');
 
 INSERT INTO PARCOURS (id_Parcours, nom_Parcours) VALUES (2, 'Parcours 2');
 COMMIT;
+-- -- Insertion dans la table CATEGORIE
+INSERT INTO CATEGORIE (id_Categorie, nom_Cat, description_Cat) 
+VALUES (1, 'Categorie 1', 'Description de la catégorie 1');
+
+INSERT INTO CATEGORIE (id_Categorie, nom_Cat, description_Cat) 
+VALUES (2, 'Categorie 2', 'Description de la catégorie 2');
+
+INSERT INTO CATEGORIE (id_Categorie, nom_Cat, description_Cat) 
+VALUES (3, 'Categorie 3', 'Description de la catégorie 3');
+
+-- Insertion dans la table ETAPE
+INSERT INTO ETAPE (code_Etape, nom_etape, latitude, longitude, niveau_confort) 
+VALUES (1, 'etape1', 'latitude1', 'longitude1', 1);
+
+INSERT INTO ETAPE (code_Etape, nom_etape, latitude, longitude, niveau_confort) 
+VALUES (2, 'etape2', 'latitude2', 'longitude2', 2);
+
+INSERT INTO ETAPE (code_Etape, nom_etape, latitude, longitude, niveau_confort) 
+VALUES (3, 'etape3', 'latitude3', 'longitude3', 3);
+
 
 -- Insertion dans la table TRONCONS
-INSERT INTO TRONCONS (id_Tr, nom_Tr, distance, Type_Sol) VALUES (1, 'Tronçon 1', '10', 'Sol 1');
-COMMIT;
-INSERT INTO TRONCONS (id_Tr, nom_Tr, distance, Type_Sol) VALUES
+INSERT INTO TRONCONS (id_Tr, nom_Tr, distance, Type_Sol, ville_tr, etape_depart, etape_arrivee) 
+VALUES (1, 'Troncon 1', 10, 'Sol 1', 'Orleans', 1, 2);
 
-(2, 'Tronçon 2', '8', 'Sol 2');
-INSERT INTO TRONCONS (id_Tr, nom_Tr, distance, Type_Sol) VALUES
+INSERT INTO TRONCONS (id_Tr, nom_Tr, distance, Type_Sol, ville_tr, etape_depart, etape_arrivee) 
+VALUES (2, 'Troncon 2', 8, 'Sol 2', 'Paris', 2, 3);
 
-(3, 'Tronçon 3', '12', 'Sol 3');
+INSERT INTO TRONCONS (id_Tr, nom_Tr, distance, Type_Sol, ville_tr, etape_depart, etape_arrivee) 
+VALUES (3, 'Troncon 3', 12, 'Sol 3', 'Orleans', 3, 1);
+-- Insertion dans la table CONTENIR
+INSERT INTO CONTENIR (id_Parcours, id_Tr) VALUES (1, 1);
+INSERT INTO CONTENIR (id_Parcours, id_Tr) VALUES (1, 2);
+INSERT INTO CONTENIR (id_Parcours, id_Tr) VALUES (2, 3);
+
+
+
+
+
+
+
